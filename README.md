edb1901799ab0616207e5b17c069ff4b687fcf7a
Eduardo Flores <@eduflornet>
2021-03-03 19:17:59 +0100

    Added Demo Module, Flexbox Demo, Set up the routes, scaffolding the App, The Sidenav Component, Styling the Sidenav, Configured the Sidenav, Added Responsiveness, Created a Toggle Button.

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # Angular-Material
 Styling Application with Angular Material
 
-Used tools:
+## Used tools:
 
-## Angular CLI: 11.2.1
-## Angular Material: 11.2.1
-## Typescript: 4.1.2
-## NodeJS: 14.15.4
+- Angular CLI: 11.2.1
+- Angular Material: 11.2.1
+- Angular Flex Layout: 11.0.0-beta.33  (FlexBox CSS + mediaQuery)
+- Typescript: 4.1.2
+- NodeJS: 14.15.4
 
-References:
-[Getting Started with Angular Material](https://material.angular.io/guide/getting-started)
-[Material Icons Guide](https://google.github.io/material-design-icons/)
+## References:
 
-
-
-
-
-
+- [Getting Started with Angular Material](https://material.angular.io/guide/getting-started)
+- [Material Icons Guide](https://google.github.io/material-design-icons/)
+- [Angular Flex Layout](https://github.com/angular/flex-layout)
+- [Layout Demos](https://tburleson-layouts-demos.firebaseapp.com/#/docs)
--- angular-material-app/src/app/app.module.ts
+++ angular-material-app/src/app/app.module.ts
@@ -1,21 +1,26 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
 
+// apply lazy loading for demo module
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
+  { path: '**', redirectTo: 'contactmanager' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
